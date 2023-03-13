3de5c1d8a64036e35ad8546970a67446632ac890
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-03-13 05:19:58 -0500

    Hooks con cypress

---
+++ cypress/e2e/hooks.cy.js
@@ -0,0 +1,39 @@
+describe("hooks", () => {
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+  });
+  beforeEach(() => {
+    cy.visit("/automation-practice-form");
+  });
+  afterEach(() => {
+    cy.visit("/");
+  });
+  it("hooks", () => {
+    cy.visit("/automation-practice-form");
+    //verificar si estoy en la pagina correcta
+    cy.url.should("include", "demoqa.com");
+    cy.get("#firstName").should("be.visible");
+    //se pueden concatenar aserciones
+    cy.get("#firstName")
+      .should("be.visible")
+      .should("have.attr", "placeholder", "First Name");
+  });
+
+  it("Aserciones 2", () => {
+    cy.visit("/automation-practice-form");
+    //element es un JQuery<HTMLElement>
+    cy.get("#firstName").then((element) => {
+      expect(element).to.be.visible;
+      expect(element).to.have.attr("placeholder", "First Name");
+    });
+  });
+
+  it("Aserciones 3", () => {
+    cy.visit("/automation-practice-form");
+    cy.get("#firstName").then((element) => {
+      assert.equal(element.attr("placeholder"), "First Name");
+    });
+  });
+});
