f6397b88f1ad5fe2da4318bc8c61ed5860d093b7
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-03-14 05:16:02 -0500

    Interactuar con el input

---
+++ cypress/e2e/interactuarInput.cy.js
@@ -0,0 +1,16 @@
+describe("hooks", () => {
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+  });
+  it("Input", () => {
+    cy.visit("/automation-practice-form");
+    cy.get("#firstName").type("Javier"); //escribir en el input
+    cy.get("#lastName").type("Fuentes");
+    //si pongo cy.get("#firstName").type('Javier'); se concatena
+    cy.get("#firstName").type("{selectAll}{backspace}"); //limpiar input
+    cy.get("#firstName").type("Jennyfer");
+    cy.get("#firstName").clear(); //limpiar input
+  });
+});
