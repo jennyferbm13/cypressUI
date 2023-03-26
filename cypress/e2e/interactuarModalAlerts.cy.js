4fe04a0fb75b2d2c31c6ef2f994be6a19c12c384
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-03-25 23:08:56 -0500

    Correciones alert

--- cypress/e2e/interactuarModalAlerts.cy.js
+++ cypress/e2e/interactuarModalAlerts.cy.js
@@ -12,35 +12,35 @@ describe("Modal y alerts", () => {
   });
 
   it("Interactuar con alert 1", () => {
-    cy.visit("/alert");
+    cy.visit("/alerts");
 
     const stub = cy.stub();
     cy.on("window:confirm", stub); //escuchando cuando haya un window confirm
-    cy.get("#confirmButtom")
+    cy.get("#confirmButton")
       .click()
       .then(() => {
         expect(stub.getCall(0)).to.be.calledWith("Do you confirm action?");
       });
 
-    cy.constains("You select Ok").should("exist");
+    cy.contains("You selected Ok").should("exist");
   });
 
   it("Interactuar con alert click OK", () => {
-    cy.visit("/alert");
+    cy.visit("/alerts");
     cy.get("#confirmButton").click();
     cy.on("window:confirm", (confirm) => {
       expect(confirm).to.equal("Do you confirm action?");
     });
-    cy.constains("Ypu selected ok").should("exist");
+    cy.contains("You selected Ok").should("exist");
   });
 
-  it("Interactuar con alert click cancel", () => {
-    cy.visit("/alert");
+  it.only("Interactuar con alert click cancel", () => {
+    cy.visit("/alerts");
     cy.get("#confirmButton").click();
     cy.on("window:confirm", (confirm) => {
       expect(confirm).to.equal("Do you confirm action?");
       return false;
     });
-    cy.constains("Ypu selected ok").should("exist");
+    cy.contains("You selected Cancel").should("exist");
   });
 });
